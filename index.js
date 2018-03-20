6050630b7b5973c0b7118e01ef7e10053dbf31b9
Kimberly Sampson <kbburk54@yahoo.com>
2018-03-20 05:26:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // write your code below!
 var name = "Susan"
-var height = "is less than 40"
+var height = "is less than parseInt(40)"
 var message = `${name} is ${height} inches tall`
 
 module.exports = {name, height, message}
